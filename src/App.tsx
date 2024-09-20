45d2157c83d2567657d361b13efc4ec624a1a3da
bernardsam-apoh <sb.bernardsam@gmail.com>
2024-09-20 12:19:47 +0100

    Added extra heading

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ function App() {
 
   return (
     <>
-        <h1>Medium Clone</h1>
+        <h1>Medium Clone and Blog Site</h1>
       <h2>An application to create a blog site for a learning purpose</h2>
     </>
   )
