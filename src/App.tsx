f6a7ddd0896e547d8bf93c1f244630e52504e25a
BernardSDev <sb.bernardsam.dev@gmail.com>
2024-09-20 13:14:43 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,8 @@ function App() {
 
   return (
     <>
-        <h1>App</h1>
+        <h1>Meduim Clone</h1>
+      <h2>An application to create a blog site for a learning purpose</h2>
     </>
   )
 }
