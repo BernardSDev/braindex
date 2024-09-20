373eb07eb4febc8ad2763a1a2370a6f47ff695be
BernardSDev <sb.bernardsam.dev@gmail.com>
2024-09-20 13:22:44 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ function App() {
     <>
         <h1>Medium Clone and Blog Site</h1>
       <h2>An application to create a blog site for a learning purpose</h2>
+      <p>lorem and more...</p>
     </>
   )
 }
