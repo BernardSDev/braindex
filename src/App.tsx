6874aae599782c896f86893b6733fbe32e27799f
bernardsam-apoh <sb.bernardsam@gmail.com>
2024-09-20 12:28:56 +0100

    Refactoring code

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,7 @@
 function App() {
 
   return (
-    <>
-        <h1>Medium Clone and Blog Site</h1>
-      <h2>An application to create a blog site for a learning purpose</h2>
-      <p>lorem and more...</p>
-    </>
+      <h1>Medium Clone</h1>
   )
 }
 
