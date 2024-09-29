4456573d9db318952dc80047f977c7c783a414a2
bernardsam-apoh <sb.bernardsam@gmail.com>
2024-09-29 22:22:38 +0100

    Setup basic structure for BlogList

---
+++ src/components/BlogListHeading.tsx
@@ -0,0 +1,5 @@
+export default function BlogListHeading() {
+    return (
+        <h1 className="bg-indigo-400 text-cyan-50 p-4">Trending articles</h1>
+    )
+}
--- src/pages/BlogList.tsx
+++ src/pages/BlogList.tsx
@@ -1,10 +1,19 @@
 import BlogItem from "../components/BlogItem.tsx";
+import Header from "../components/Header.tsx";
+import Footer from "../components/Footer.tsx";
+import BlogListHeading from "../components/BlogListHeading.tsx";
 
 export default function BlogList() {
     return (
         <div>
-            <h1>All Blogs</h1>
-            <BlogItem />
+            <Header />
+            <BlogListHeading />
+            <main className="p-2 bg-orange-100">
+                <BlogItem />
+                <BlogItem />
+                <BlogItem />
+            </main>
+            <Footer />
         </div>
     )
 }
