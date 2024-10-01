0ed425074cf921108aec37e6769ba1e6f5404b10
bernardsam-apoh <sb.bernardsam@gmail.com>
2024-10-01 16:18:56 +0100

    Remove commented codes in supabase.ts

--- src/services/supabase.ts
+++ src/services/supabase.ts
@@ -1,9 +1,6 @@
 
 import { createClient } from '@supabase/supabase-js'
 
-// const supabaseUrl = process.env.REACT_APP_SUPABASE_URL as string;
-// const supabaseKey = process.env.REACT_APP_SUPABASE_KEY as string;
-
 export const supabaseUrl = import.meta.env.VITE_APP_SUPABASE_URL
 export const supabaseKey = import.meta.env.VITE_APP_SUPABASE_KEY
 
