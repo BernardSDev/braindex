f6ebb277c0aff3162b228a7bfa7fd1a25cc3cdb1
bernardsam-apoh <sb.bernardsam@gmail.com>
2024-10-02 01:55:45 +0100

    Implement redirect from homepage to blogs page

--- src/components/BlogSection.tsx
+++ src/components/BlogSection.tsx
@@ -1,12 +1,13 @@
 import BlogSummary from "./BlogSummary.tsx";
 import {IBlog} from "../interfaces/Blog.ts";
 import ShowMore from "./ShowMore.tsx";
+import { Link } from "react-router-dom";
 
 export default function BlogSection({ blogs } : { blogs:IBlog[] | undefined }) {
     return (
         <section className="blog-section bg-violet-500 px-4 py-6">
             <div className="bg-amber-50 p-2 mb-4 mt-3">
-                <h1 className="underline mb-2">view all blogs</h1>
+                <Link to={`/blogs`} className="underline mb-2">view all blogs</Link>
                 <h1 className="capitalize font-semibold mb-2">read wide to enrich your knowledge</h1>
             </div>
             <div className="featured-post">
