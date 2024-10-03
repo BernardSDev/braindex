e9413228b79f44c1965b4e65204900a4a36771c3
bernardsam-apoh <sb.bernardsam@gmail.com>
2024-10-03 01:53:55 +0100

    Setup header  NavLinks

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,17 @@
+import { NavLink } from "react-router-dom";
+
 export default function Header() {
     return(
-        <header className="bg-indigo-700 text-white p-2 py-6 header flex items-center justify-between w-full">
-            <div className="logo text-red-300">Braindex</div>
-            <nav className="flex items-center gap-4 justify-between">
-                <a href="">Blogs</a>
-                <a href="">Add new</a>
-                <a href="">Contact</a>
+        <header className="bg-white text-white mb-2 border-amber-950 p-2 py-6 header flex items-center justify-between w-full">
+            <div className="logo text-black">
+                <NavLink to="/">BrainDex</NavLink>
+            </div>
+            <nav >
+                <ul className="flex items-center gap-4 justify-between text-black">
+                    <li><NavLink to="/">Home</NavLink></li>
+                    <li><NavLink to="/blogs">Blogs</NavLink></li>
+                    <li><NavLink to="/form">Add new</NavLink></li>
+                </ul>
             </nav>
         </header>
     )
