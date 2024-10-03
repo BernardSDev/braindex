c331bfcc33d16b87b4458e8ba63e37f22deb106b
bernardsam-apoh <sb.bernardsam@gmail.com>
2024-10-03 15:53:45 +0100

    fix: readting time at Blog.tsx

--- src/pages/Blog.tsx
+++ src/pages/Blog.tsx
@@ -3,6 +3,7 @@ import { useParams } from "react-router-dom";
 import { useBlogs } from "../context/BlogsContext.tsx";
 import Header from "../components/Header.tsx";
 import Footer from "../components/Footer.tsx";
+import {readingTime} from "../helpers/util.ts";
 
 export default function Blog() {
     const { id } = useParams();
@@ -20,7 +21,7 @@ export default function Blog() {
                         <img src={blog?.avatar} alt={blog?.avatar} width={35} height={35} className="w-12 h-12 object-cover rounded-full"/>
                         <div className="bg-green-500x">
                             <div className="font-semibold text-lg tracking-tighter ">{blog?.author}</div>
-                            <div className="text-sm">Published on <span>{moment(blog?.created_at).format('LL')}</span> - <span>3 min read</span></div>
+                            <div className="text-sm">Published on <span>{moment(blog?.created_at).format('LL')}</span> - <span>{readingTime(blog?.content)} min read</span></div>
                         </div>
                     </div>
                     <img src={blog?.contentImage} alt={blog?.contentImage} className="mb-12 h-[30vh] md:h-[55vh] w-full"/>
