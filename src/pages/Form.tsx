0793ef0268f6f6c6ba86923551778f8ed959b1ea
bernardsam-apoh <sb.bernardsam@gmail.com>
2024-10-03 20:51:36 +0100

    feature: implement inputs disabling at Form.tsx

--- src/pages/Form.tsx
+++ src/pages/Form.tsx
@@ -20,7 +20,7 @@ export default function Form() {
 
     const queryClient = useQueryClient();
 
-    const { mutate, isPending } = useMutation({
+    const { mutate, isPending:isCreating } = useMutation({
         mutationFn: createBlog,
         onSuccess: () => {
             toast.success("New blog successfully created");
@@ -41,58 +41,58 @@ export default function Form() {
             <form className="bg-green-500x p-4 grid grid-cols-1 gap-4" onSubmit={handleSubmit(onSubmit)}>
                 <div className="md:w-1/3">
                     <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="author">Author</label>
-                    <input {...register("author",)} id="author" type="text" placeholder="Author"
-                           className="bg-blue-50 appearance-none rounded w-full py-2 px-4 text-gray-700 leading-tight focus:border focus:outline-none focus:bg-white focus:border-purple-500"/>
+                    <input {...register("author",)} disabled={isCreating} id="author" type="text" placeholder="Author"
+                           className={`${isCreating ? 'bg-gray-400' : 'bg-blue-50'} appearance-none rounded w-full py-2 px-4 text-gray-700 leading-tight focus:border focus:outline-none focus:bg-white focus:border-purple-500`} />
                     {errors.author && (<div className="text-white py-1 bg-red-700">{errors.author.message}</div>)}
                 </div>
 
                 <div className="md:w-1/3">
                     <label className="block text-gray-700 text-sm font-bold mb-2"
                            htmlFor="profession">Profession</label>
-                    <input {...register("profession",)} id="profession" type="text" placeholder="Profession"
-                           className="bg-blue-50 appearance-none rounded w-full py-2 px-4 text-gray-700 leading-tight focus:border focus:outline-none focus:bg-white focus:border-purple-500"/>
+                    <input {...register("profession",)} disabled={isCreating} id="profession" type="text" placeholder="Profession"
+                           className={`${isCreating ? 'bg-gray-400' : 'bg-blue-50'} appearance-none rounded w-full py-2 px-4 text-gray-700 leading-tight focus:border focus:outline-none focus:bg-white focus:border-purple-500`} />
                     {errors.profession && (
                         <div className="text-white py-1 bg-red-700">{errors.profession.message}</div>)}
                 </div>
 
                 <div className="md:w-1/3">
                     <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="title">Title</label>
-                    <input {...register("title",)} id="title" type="text" placeholder="Title"
-                           className="bg-blue-50 appearance-none rounded w-full py-2 px-4 text-gray-700 leading-tight focus:border focus:outline-none focus:bg-white focus:border-purple-500"/>
+                    <input {...register("title",)} disabled={isCreating} id="title" type="text" placeholder="Title"
+                           className={`${isCreating ? 'bg-gray-400' : 'bg-blue-50'} appearance-none rounded w-full py-2 px-4 text-gray-700 leading-tight focus:border focus:outline-none focus:bg-white focus:border-purple-500`} />
                     {errors.title && (<div className="text-white py-1 bg-red-700">{errors.title.message}</div>)}
                 </div>
 
                 <div className="md:w-1/3">
                     <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="excerpt">Excerpt</label>
-                    <input {...register("excerpt",)} id="excerpt" type="text" placeholder="Excerpt"
-                           className="bg-blue-50 appearance-none rounded w-full py-2 px-4 text-gray-700 leading-tight focus:border focus:outline-none focus:bg-white focus:border-purple-500"/>
+                    <input {...register("excerpt",)} disabled={isCreating} id="excerpt" type="text" placeholder="Excerpt"
+                           className={`${isCreating ? 'bg-gray-400' : 'bg-blue-50'} appearance-none rounded w-full py-2 px-4 text-gray-700 leading-tight focus:border focus:outline-none focus:bg-white focus:border-purple-500`}/>
                     {errors.excerpt && (<div className="text-white py-1 bg-red-700">{errors.excerpt.message}</div>)}
                 </div>
 
                 <div className="md:w-1/3">
                     <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="content">Content</label>
-                    <input {...register("content",)} id="content" type="text" placeholder="Content"
-                           className="bg-blue-50 appearance-none rounded w-full py-2 px-4 text-gray-700 leading-tight focus:border focus:outline-none focus:bg-white focus:border-purple-500"/>
+                    <input {...register("content",)} disabled={isCreating} id="content" type="text" placeholder="Content"
+                           className={`${isCreating ? 'bg-gray-400' : 'bg-blue-50'} appearance-none rounded w-full py-2 px-4 text-gray-700 leading-tight focus:border focus:outline-none focus:bg-white focus:border-purple-500`} />
                     {errors.content && (<div className="text-white py-1 bg-red-700">{errors.content.message}</div>)}
                 </div>
 
                     <div className="md:w-1/3">
                         <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="avatar">Avatar</label>
-                        <input {...register("avatar",)} id="avatar" accept="image/*" type="file" placeholder="Avatar"
-                               className="bg-blue-50 appearance-none rounded w-full py-2 px-4 text-gray-700 leading-tight focus:border focus:outline-none focus:bg-white focus:border-purple-500"/>
+                        <input {...register("avatar",)} disabled={isCreating} id="avatar" accept="image/*" type="file" placeholder="Avatar"
+                               className={`${isCreating ? 'bg-gray-400' : 'bg-blue-50'}  appearance-none rounded w-full py-2 px-4 text-gray-700 leading-tight focus:border focus:outline-none focus:bg-white focus:border-purple-500`}/>
                         {errors.avatar && (<div className="text-white py-1 bg-red-700">{errors.avatar.message}</div>)}
                     </div>
                     <div className="md:w-1/3">
                         <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="contentImage">Content
                             image</label>
-                        <input {...register("contentImage",)} id="contentImage" accept="image/*" type="file"
-                               placeholder="Content image" className="bg-blue-50 appearance-none rounded w-full py-2 px-4 text-gray-700 leading-tight focus:border focus:outline-none focus:bg-white focus:border-purple-500" />
+                        <input {...register("contentImage",)} disabled={isCreating} id="contentImage" accept="image/*" type="file"
+                               placeholder="Content image" className={`${isCreating ? 'bg-gray-400' : 'bg-blue-50'} appearance-none rounded w-full py-2 px-4 text-gray-700 leading-tight focus:border focus:outline-none focus:bg-white focus:border-purple-500`} />
                         {errors.contentImage && (<div className="text-white py-1 bg-red-700">{errors.contentImage.message}</div>)}
                     </div>
 
                     <div className="border-t-2 mt-6 mb-2 border-t-gray-300"></div>
-                <button type="submit" className="bg-black py-3 text-white">
-                    {isPending ? "Creating..." : "Create"}
+                <button type="submit" disabled={isCreating} className={`py-3 text-white ${isCreating ? 'bg-gray-500' : 'bg-black' }`}>
+                    {isCreating ? "Creating..." : "Create"}
                 </button>
             </form>
         </div>
