9e5866d7e5b0ff9497aa142bbdf52801eac29a22
bernardsam-apoh <sb.bernardsam@gmail.com>
2024-10-03 15:41:50 +0100

    style: rename add-new to write with an icon

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,22 @@
 import { NavLink } from "react-router-dom";
 
+
 export default function Header() {
     return(
         <header className="header flex items-center container py-6 px-4 mx-auto justify-between">
             <div className="logo text-3xl font-bold tracking-tighter">
                 <NavLink to="/">BrainDex</NavLink>
             </div>
-            <nav >
-                <ul className="flex items-center justify-between gap-2 text-black">
+            <nav>
+                <ul className="flex items-center gap-8">
                     <li><NavLink to="/">Home</NavLink></li>
-                    <li><NavLink to="/form">Add new</NavLink></li>
+                    <li className="flex">
+                        <NavLink to="/form">
+                        <span className="flex gap-0.5">
+                            <img src="/src/assets/images/write.svg" width={35} height={35} className="text-red-600 w-6 h-6 "  alt="write"/>write
+                        </span>
+                    </NavLink>
+                    </li>
                 </ul>
             </nav>
         </header>
