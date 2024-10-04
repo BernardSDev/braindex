1379a763068de7bff78b22bb804548e97e376c4f
bernardsam-apoh <sb.bernardsam@gmail.com>
2024-10-04 06:38:59 +0100

    update: change company name to BrainDex

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -44,7 +44,7 @@ export default function Footer() {
             </div>
 
             <div className="border-t border-gray-700 mt-8 pt-4 text-center">
-                <p className="text-gray-500">&copy; {new Date().getFullYear()} Company Name. All Rights Reserved.</p>
+                <p className="text-gray-500">&copy; {new Date().getFullYear()} BrainDex. All Rights Reserved.</p>
             </div>
         </footer>
     );
