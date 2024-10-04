0787ff3524bf49218a5ccf0fd451e99c4c141a75
bernardsam-apoh <sb.bernardsam@gmail.com>
2024-10-04 14:30:36 +0100

    update: move images from src to public

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -2,7 +2,7 @@ export default function HeroSection() {
     return (
         <section className="hero mb-6">
             <div className="hero-banner">
-                <img src="/src/assets/images/hero-banner.jpg" alt="Featured Post"
+                <img src="/public/images/hero-banner.jpg" alt="Featured Post"
                      className="w-full h-[30vh] md:h-[50vh] object-cover"/>
             </div>
         </section>
