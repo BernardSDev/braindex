d304bcefa1763c1b98bea4e7cb1cf035a689044b
bernardsam-apoh <sb.bernardsam@gmail.com>
2024-10-13 00:25:52 +0100

    update: improve appearance at Blog.tsx

--- src/pages/Blog.tsx
+++ src/pages/Blog.tsx
@@ -17,8 +17,8 @@ export default function Blog() {
             <Header />
             <div className="bg-red-900x py-10 container border-t border-t-black mx-auto">
                 <main className="bg-red-200x px-6">
-                    <div className="text-2xl leading-snug text-black mb-10 font-semibold tracking-tighter capitalize md:text-center">{blog?.title}</div>
-                    <div className="text-lg md:text-center mb-8">{blog?.excerpt}</div>
+                    <div className="text-3xl leading-snug text-black mb-10 font-semibold tracking-tighter capitalize md:text-center">{blog?.title}</div>
+                    <div className="text-xl md:text-center mb-8">{blog?.excerpt}</div>
                     <div className="bg-orange-800x mb-8 flex gap-x-6">
                         <img src={blog?.avatar} alt={blog?.avatar} width={35} height={35} className="w-12 h-12 object-cover rounded-full"/>
                         <div className="">
@@ -27,7 +27,7 @@ export default function Blog() {
                         </div>
                     </div>
                     <img src={blog?.contentImage} alt={blog?.contentImage} className="mb-12 h-[30vh] md:h-[55vh] w-full"/>
-                    <div className="text-lg text-[#6D6D6D] leading-normal" dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(blog?.content ?? "") }} />   
+                    <div className="text-xl text-[#6D6D6D] leading-snug" dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(blog?.content ?? "") }} />   
                 </main>
             </div>
             <Footer/>
